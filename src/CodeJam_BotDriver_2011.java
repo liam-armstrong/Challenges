d7c8af1e2bbaddb3f2cdce834bb143453254e620
Liam Armstrong <Liam Armstrong@192.168.2.15>
2016-09-29 22:46:02 -0400

    Finalized ParseInput method, changed command storage to object vs global

--- src/CodeJam_BotDriver_2011.java
+++ src/CodeJam_BotDriver_2011.java
@@ -1,4 +1,32 @@
+import java.util.*;
 
 public class CodeJam_BotDriver_2011 {
+	//creates ArrayList to hold bots
+	static ArrayList<CodeJam_BotObj_2011> master = new ArrayList<CodeJam_BotObj_2011>();
 
+	public static void main(String[] args) {
+		ParseInput();
+		System.out.println(master.get(0).getNextCmd());
+		System.out.println(master.get(1).getNextCmd());
+	}
+
+	public static void ParseInput() {
+		Scanner in = new Scanner(System.in);
+		int x = in.nextInt() * 2;
+		for(int i = 0; i < x; i += 2){
+			int y = in.nextInt();
+			System.out.println("Added 2 Bots");
+			master.add(new CodeJam_BotObj_2011());
+			master.add(new CodeJam_BotObj_2011());
+			for(int j = 0; j < y; j++){
+				if(in.next().equalsIgnoreCase("O")){
+					master.get(i).addCmd(in.nextInt());
+				}
+				else {
+					master.get(i + 1).addCmd(in.nextInt());
+				}
+			}
+		}
+		in.close();
+	}
 }
--- src/CodeJam_BotObj_2011.java
+++ src/CodeJam_BotObj_2011.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
 
 public class CodeJam_BotObj_2011 {
-	public CodeJam_BotConstructor_2011(String color, int pos){
-		
+	int pos = 1;
+	ArrayList<Integer> cmds = new ArrayList<Integer>();
+	
+	public CodeJam_BotObj_2011(){
+	}
+	
+	public void reset(){
+		pos = 1;
+		cmds.clear();
+	}
+	
+	public int getPos(){
+		return pos;
+	}
+	
+	public void move(int dist){
+		pos += dist;
+	}
+	
+	public void press(){
+		cmds.remove(0);
+	}
+	
+	public void addCmd(int pos){
+		cmds.add(pos);
+	}
+	
+	public int getNextCmd(){
+		return cmds.get(0);
 	}
 }
