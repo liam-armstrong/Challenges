6242fbf6b7b7b6face6cfd61b19ad629d3da4757
liam-armstrong <liam.armstrong124@gmail.com>
2016-10-05 22:32:57 -0400

    Started ParseInput method

---
+++ src/CodeJam_Magicka_2011.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CodeJam_Magicka_2011 {
+	static ArrayList<ArrayList<String>> elements = new ArrayList<ArrayList<String>>();
+	public static void main(String[] args) {
+
+	}
+	
+	public static void ParseInput() {
+		Scanner in = new Scanner(System.in);
+		for(int i = 0; i < in.nextInt(); i++){
+			elements.add(new ArrayList);
+			for(int j = 0; j < in.nextInt(); j++){
+			}
+		}
+		in.close();
+	}
+
+}
